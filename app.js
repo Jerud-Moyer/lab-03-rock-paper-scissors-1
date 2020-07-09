275a579f9d9a45241c1024b3073f5ac1908d4bed
warrioryoko <miangpellegri@gmail.com>
2020-07-08 17:56:05 -0700

    Got getRandomThrow and checkResults functions, and tests for checkResults working

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
-
+import { getRandomThrow, checkResults } from './get-random-throw.js'
 // initialize state
 
 // set event listeners to update state and DOM
+
+const playerRandRoll = getRandomThrow();
+const computerRandRoll = getRandomThrow();
+console.log("Player choice is " + playerRandRoll);
+console.log("Computer choice is " + computerRandRoll);
+
+const results = checkResults(playerRandRoll, computerRandRoll);
+console.log("The result is " + results);
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,147 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { checkResults } from '../get-random-throw.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('it should take two identical strings - rock and rock - and return draw for a tie', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const playerChoice = "rock";
+    const computerChoice = "rock";
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const expected = "draw";
+    const actual = checkResults(playerChoice, computerChoice);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should take two identical strings - paper and paper - and return draw for a tie', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const playerChoice = "paper";
+    const computerChoice = "paper";
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const expected = "draw";
+    const actual = checkResults(playerChoice, computerChoice);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should take two identical strings - scissors and scissors - and return draw for a tie', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const playerChoice = "scissors";
+    const computerChoice = "scissors";
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const expected = "draw";
+    const actual = checkResults(playerChoice, computerChoice);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should take strings for rock and scissors, and return win', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const playerChoice = "rock";
+    const computerChoice = "scissors";
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const expected = "win";
+    const actual = checkResults(playerChoice, computerChoice);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should take strings for rock and paper, and return lose', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const playerChoice = "rock";
+    const computerChoice = "paper";
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const expected = "lose";
+    const actual = checkResults(playerChoice, computerChoice);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should take strings for paper and rock, and return win', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const playerChoice = "paper";
+    const computerChoice = "rock";
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const expected = "win";
+    const actual = checkResults(playerChoice, computerChoice);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should take strings for paper and scissors, and return lose', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const playerChoice = "paper";
+    const computerChoice = "scissors";
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const expected = "lose";
+    const actual = checkResults(playerChoice, computerChoice);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should take strings for scissors and rock, and return lose', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const playerChoice = "scissors";
+    const computerChoice = "rock";
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const expected = "lose";
+    const actual = checkResults(playerChoice, computerChoice);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should take strings for scissors and paper, and return win', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const playerChoice = "scissors";
+    const computerChoice = "paper";
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const expected = "win";
+    const actual = checkResults(playerChoice, computerChoice);
 
     //Expect
     // Make assertions about what is expected versus the actual result
